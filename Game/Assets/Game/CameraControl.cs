2222c9454446cbf37461c1964c8746693ba6818f
Peter Southward <petersouthward@hotmail.com>
2016-04-28 21:29:05 +0100

    added a zoom to the camera controls. set to be Overhead camera

--- Game/Assets/Game/CameraControl.cs
+++ Game/Assets/Game/CameraControl.cs
@@ -3,7 +3,12 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
-    public float Scroll_Speed = 5;
+    public float Scroll_Speed = 3;
+    public float Scroll_Shift_Speed = 5;
+    public float Camera_Zoom_Step = 2;
+
+    public float Min_Zoom_Dist = 2, Max_Zoom_Dist = 75;
+
 
 	// Use this for initialization
 	void Start () {
@@ -16,9 +21,17 @@ public class CameraControl : MonoBehaviour {
         Vector3 movedirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
 
         movedirection.Normalize();
-        movedirection *= Scroll_Speed;
+        movedirection *= (Input.GetButton("CameraSpeedUp")) ?  Scroll_Shift_Speed : Scroll_Speed;
+
+        movedirection.y = Input.GetAxis("Mouse ScrollWheel") * Camera_Zoom_Step;
 
         transform.position += movedirection;
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, Min_Zoom_Dist, Max_Zoom_Dist), transform.position.z);
+
+        
+
+
 	
 	}
 }
