a44a85db2b5ca413729900aaf7c54f35d186252c
peter southward <petersouthward@hotmail.com>
2016-05-05 10:56:59 +0100

    added base files for the AStart pathfinder currently

---
+++ Game/Assets/PathFinder/AStar.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AStar {
+
+    struct AStarNodes
+    {
+       public Node NodeInfo;
+       public float DistanceGone;
+       public float Distance2Go;
+
+    }
+
+    public IEnumerator GetPath(Vector2 MapPosStart, Vector2 MapPosEnd, int Maxsteps, float TimePerframe, uint ID)
+    {
+        List<Node> result = new List<Node>(); 
+        List<AStarNodes> OpenList = new List<AStarNodes>();
+        List<AStarNodes> CloseList = new List<AStarNodes>();
+
+        AStarNodes startNode = new AStarNodes();
+
+        startNode.NodeInfo.MapPos = MapPosStart;
+        startNode.DistanceGone = 0;
+        startNode.Distance2Go = GetDirectDistance(MapPosStart,MapPosEnd);
+        OpenList.Add(startNode);
+
+        while(OpenList.Count >0)
+        {
+            // do stuff and Yeild on Timeperframe
+        }
+
+
+
+
+
+
+        //output
+        path theWay = new path();
+        theWay.FoundPath = result;
+        theWay.isPathFound = true;
+        PathFinder.Paths[ID] = theWay;
+    }
+
+
+
+    private float GetDirectDistance(Vector2 pointA, Vector2 pointB)
+    {
+        return (pointA - pointB).magnitude;
+    }
+
+
+}
---
+++ Game/Assets/PathFinder/PathFinder.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+struct  Node
+{
+    public Vector2 MapPos;
+    //public Node? NextNode;
+    //public Node? PrevNode;
+};
+
+struct path
+{
+    public List<Node> FoundPath;
+    public bool isPathFound;
+
+};
+
+
+
+public class PathFinder : MonoBehaviour {
+
+    // this is a bridge class to call the Path Finding Functions 
+    AStar AStar = new AStar();
+
+   public static Map CurrentMap = null;
+
+    public static Dictionary<uint,path> Paths;
+
+    private uint count = 0;
+
+    void Start () {
+        CurrentMap = GetComponent<Map>();
+	}
+
+
+
+    public uint GetPath(Vector2 MapPosStart, Vector2 MapPosEnd, int Maxsteps = int.MaxValue, float TimePerframe = float.MaxValue)
+    {
+        uint ID = count;
+        ++count;
+
+        path temp = new path();
+        temp.isPathFound = false;
+        Paths.Add(ID,temp);
+
+        StartCoroutine(AStar.GetPath(MapPosStart, MapPosEnd, Maxsteps, TimePerframe, ID));
+
+        return ID;
+    }
+
+
+
+
+}
