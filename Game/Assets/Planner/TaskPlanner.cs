d5dd00859acead3703d7965c179ae918ee8de504
Rob Nicholds <robert.james.nicholds@googlemail.com>
2016-05-13 12:47:19 +0100

    Added TaskPlanner class
    class for interacting with executive

---
+++ Game/Assets/Planner/TaskPlanner.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+public class TaskPlanner : MonoBehaviour {
+
+    private Process ProcessPlanner;
+    private string working_directory = Application.dataPath;
+
+    private string PDDLDomainName = @"Domain";
+    private string PDDLProblemName = @"Problem";
+    private string SolutionName = @"/ffSolution.soln";
+
+    private string solution = "";
+
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void RunPlanner()
+    {
+        ProcessPlanner = new Process();
+
+        ProcessPlanner.StartInfo.WorkingDirectory = working_directory + @"/Planner";
+        ProcessPlanner.StartInfo.FileName = working_directory + @"/metric-ff.exe";
+        ProcessPlanner.StartInfo.Arguments = string.Format("-o {0}.pddl -f {1}.pddl", PDDLDomainName, PDDLProblemName);
+        ProcessPlanner.StartInfo.CreateNoWindow = true;
+        ProcessPlanner.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+        ProcessPlanner.Start();
+        ProcessPlanner.WaitForExit();
+    }
+
+    void ReadSolution()
+    {
+        var result = File.ReadAllLines(SolutionName).Where(s => s.Contains(":"));
+
+        solution = result.ToList()[0].ToString();
+
+        File.Delete(SolutionName);
+        
+    }
+}
