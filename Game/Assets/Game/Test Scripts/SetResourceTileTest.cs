19c6775b50feba341d634e1e717a66d7804182a2
Peter Southward <petersouthward@hotmail.com>
2016-05-14 21:58:05 +0100

    same as lst commit

---
+++ Game/Assets/Game/Test Scripts/SetResourceTileTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetResourceTileTest : MonoBehaviour
+{
+
+    public int resourceAmount = 0;
+    public ResourceType type;
+
+    public int SpawnX = 0, SpawnY = 0;
+
+
+    public float DelayTime = 1f;
+
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(DelayTime);
+
+        IVec2 MapPos = new IVec2(SpawnX, SpawnY);
+
+        Map.CurrentMap.setResourcetile(type, resourceAmount, MapPos);
+    }
+}
+	
