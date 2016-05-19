095e690fcbac064659a44324a3a9c764d6e7fb9e
Will Marshall <wwwolfie13@gmail.com>
2016-05-19 17:52:27 +0100

    Split IVec2 out
    As it should have been always you absolute madman.

--- Game/Assets/PathFinder/PathFinder.cs
+++ Game/Assets/PathFinder/PathFinder.cs
@@ -18,41 +18,6 @@ public struct path
     public bool isPathFound{get;set;}
 };
 
-[System.Serializable]
-public class IVec2
-{
-    public IVec2(){x = 0; y = 0;}
-    public IVec2(int newx, int newy) { x = newx; y = newy; }
-    public IVec2(IVec2 newVec) { x = newVec.x; y = newVec.y; }
-
-    public static IVec2 operator +(IVec2 a, IVec2 b) { return new IVec2(a.x + b.x, a.y + b.y); }
-    public static IVec2 operator -(IVec2 a, IVec2 b) { return new IVec2(a.x - b.x, a.y - b.y); }
-    public static IVec2 operator *(IVec2 a, int b) { return new IVec2(a.x * b, a.y * b); }
-    public static bool operator ==(IVec2 a, IVec2 b) { return (a.x==b.x && a.y == b.y);}
-    public static bool operator !=(IVec2 a, IVec2 b) { return !(a==b); }
-
-    public float magnitude() { return Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(y, 2)); }
-
-    public override string ToString() { return "(" + x + ", " + y + ")"; }
-
-    public int x, y;
-
-	public override bool Equals(System.Object o)
-	{
-		return o.GetType().Equals(this.GetType ()) && (o as IVec2) == this;
-	}
-
-	public override int GetHashCode ()
-	{
-		return base.GetHashCode ();
-	}
-
-	public int manhatttanDistance(IVec2 other)
-	{
-		return Mathf.Abs(other.x - this.x) + Mathf.Abs(other.y - this.y);
-	}
-}
-
 
 public class PathFinder {
 
